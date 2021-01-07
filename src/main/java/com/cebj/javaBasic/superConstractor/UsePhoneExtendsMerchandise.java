8e64b051d5da457e7732e7c7e11d1d7887a36e22
zjh <501508468@qq.com>
2021-01-07 20:13:23 +0800

    super调用父类的构造函数

---
+++ src/main/java/com/cebj/javaBasic/superConstractor/UsePhoneExtendsMerchandise.java
@@ -0,0 +1,10 @@
+package com.cebj.javaBasic.superConstractor;
+
+import com.cebj.javaBasic.superConstractor.supermarket.Phone;
+
+public class UsePhoneExtendsMerchandise {
+    public static void main(String[] args) {
+        Phone phone = new Phone();
+        phone.describe();
+    }
+}
